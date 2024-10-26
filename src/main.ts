d3ced1a5f0c39b3e4c8b403f06b284008ee64fd3
Carlos6464 <adrianosodre23@gmail.com>
2024-10-26 12:44:47 -0300

    cors false

--- src/main.ts
+++ src/main.ts
@@ -12,19 +12,6 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   // Cria a aplicação NestJS com o módulo principal
   const app = await NestFactory.create(AppModule, {cors: false});
-
-  // // Configurando CORS
-  // app.enableCors({
-  //   origin: 'https://frontend-pizzaria-sigma.vercel.app', // Permitir requisições apenas desse domínio
-  //   methods: 'GET,POST,PUT,DELETE,OPTIONS', // Métodos HTTP permitidos
-  //   allowedHeaders: 'Content-Type, Authorization', // Cabeçalhos permitidos
-  //   exposedHeaders: 'Authorization', // Cabeçalhos que podem ser expostos ao frontend (opcional)
-  //   credentials: true, // Permitir cookies e credenciais
-  //   preflightContinue: false, // Para não permitir que o pré-vôo prossiga para o próximo middleware
-  //   optionsSuccessStatus: 204 // Código de status para resposta de sucesso ao pré-vôo (OPTIONS)
-  // });
-
-
   
 
   // configurando o swagger para gerar documentação
